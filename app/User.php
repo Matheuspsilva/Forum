b183d148f01a199672daaf38171705f8cb7bf39e
Matheus <matheuspsilva29@gmail.com>
2021-12-09 15:58:04 -0300

    Mapping models

--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function thread()
+    {
+        return $this->hasMany(Thread::class);
+    }
+
 }
